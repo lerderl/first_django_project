33f37b2858bc7bccd9dc7cefd99de0fb24a41483
JOSEPH <josepholukunle1107@gmail.com>
2021-04-26 15:01:37 +0100

    Added some tests.

--- my_app/urls.py
+++ my_app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path("index/", views.index, name="index"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("", BlogListView.as_view(), name="home")
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -12,3 +12,8 @@ def index(request):
 class BlogListView(ListView):
     model = Post
     template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
