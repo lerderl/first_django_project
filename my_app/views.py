0900a53639abc865abebbfd9049b940cd407a835
JOSEPH <josepholukunle1107@gmail.com>
2021-04-26 16:07:31 +0100

    Added a create form feature.

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
@@ -17,3 +18,9 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
