871de80d5089afa29576112379ef5474d3511f95
JOSEPH <josepholukunle1107@gmail.com>
2021-04-27 12:41:29 +0100

    Added password change and reset features.

--- first_django_project/settings.py
+++ first_django_project/settings.py
@@ -135,3 +135,5 @@ django_heroku.settings(locals())
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
